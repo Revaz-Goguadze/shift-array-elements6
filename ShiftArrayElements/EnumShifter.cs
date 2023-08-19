abb271bd5db08bdd2a30a737ea4965c6094c379a
Revaz-Goguadze <goguadze.rezi2022@gmail.com>
2023-08-19 20:05:17 +0400

    dasdasda

--- ShiftArrayElements/EnumShifter.cs
+++ ShiftArrayElements/EnumShifter.cs
@@ -12,8 +12,55 @@
         /// <exception cref="InvalidOperationException">direction array contains an element that is not <see cref="Direction.Left"/> or <see cref="Direction.Right"/>.</exception>
         public static void Shift(int[] source, Direction[] directions)
         {
-            // TODO #1. Implement the method using "for" statements and indexers only (don't use Array.Copy method here).
-            throw new NotImplementedException();
+            if (source.Length == 0 || directions.Length == 0)
+            {
+                return;
+            }
+
+            int length = source.Length;
+
+            for (int i = 0; i < directions.Length; i++)
+            {
+                Direction currentDirection = directions[i];
+
+                switch (currentDirection)
+                {
+                    case Direction.Left:
+                        ShiftLeft(source, length);
+                        break;
+
+                    case Direction.Right:
+                        ShiftRight(source, length);
+                        break;
+
+                    default:
+                        throw new InvalidOperationException($"Incorrect {currentDirection} enum value.");
+                }
+            }
+        }
+
+        private static void ShiftLeft(int[] array, int length)
+        {
+            int temp = array[0];
+
+            for (int i = 0; i < length - 1; i++)
+            {
+                array[i] = array[i + 1];
+            }
+
+            array[length - 1] = temp;
+        }
+
+        private static void ShiftRight(int[] array, int length)
+        {
+            int temp = array[length - 1];
+
+            for (int i = length - 1; i > 0; i--)
+            {
+                array[i] = array[i - 1];
+            }
+
+            array[0] = temp;
         }
     }
 }
--- ShiftArrayElements/Shifter.cs
+++ ShiftArrayElements/Shifter.cs
@@ -11,8 +11,46 @@
         /// <exception cref="ArgumentNullException">iterations array is null.</exception>
         public static void Shift(int[] source, int[] iterations)
         {
-            // TODO #2. Implement the method using "for" statements and Array.Copy method.
-            throw new NotImplementedException();
+            if (source.Length == 0 || iterations.Length == 0)
+            {
+                return;
+            }
+
+            int length = source.Length;
+
+            for (int i = 0; i < iterations.Length; i++)
+            {
+                int currentIteration = iterations[i];
+
+                if (i % 2 == 0)
+                {
+                    ShiftLeft(source, length, currentIteration);
+                }
+                else
+                {
+                    ShiftRight(source, length, currentIteration);
+                }
+            }
+        }
+
+        private static void ShiftLeft(int[] array, int length, int iterations)
+        {
+            for (int i = 0; i < iterations; i++)
+            {
+                int temp = array[0];
+                Array.Copy(array, 1, array, 0, length - 1);
+                array[length - 1] = temp;
+            }
+        }
+
+        private static void ShiftRight(int[] array, int length, int iterations)
+        {
+            for (int i = 0; i < iterations; i++)
+            {
+                int temp = array[length - 1];
+                Array.Copy(array, 0, array, 1, length - 1);
+                array[0] = temp;
+            }
         }
     }
 }
